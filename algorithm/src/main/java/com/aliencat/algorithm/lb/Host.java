6c2799d0fac0d6fad8cc0d66a4544fde6f1de3e2
HelloWorld19930603 <784264581@qq.com>
2021-05-05 22:18:06 +0800

    负载均衡之随机算法

--- algorithm/src/main/java/com/aliencat/algorithm/lb/Host.java
+++ algorithm/src/main/java/com/aliencat/algorithm/lb/Host.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 public class Host {
 
-    private static List<String> hostList = new ArrayList<>();
+    private static List<String> hostList;
 
     static {
+        //初始化主机ip地址
         initHostList();
     }
 
     public static void initHostList(){
+        hostList = new ArrayList<>();
         hostList.add("192.168.0.1");
         hostList.add("192.168.0.2");
         hostList.add("192.168.0.3");
