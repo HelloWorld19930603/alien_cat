2e7f907726f16114fd48d980257275dfdf2ebf94
chengcheng5 <chengcheng5@asiainfo.com>
2021-05-12 18:30:49 +0800

    测试框架powermock

--- javabase/src/src/main/java/com/aliencat/javabase/api/string/StringContrast.java
+++ javabase/src/src/main/java/com/aliencat/javabase/api/string/StringContrast.java
@@ -1,13 +1,11 @@
 package com.aliencat.javabase.api.string;
 
 
-import org.junit.Test;
-
 /**
- * - String**：String对象是不可变的。对String对象的任何改变都不影响到原对象的内容，
- * 相关的任何change操作都会导致该字符串变量指向新的对象的地址。
- * - StringBuilder**：StringBuilder是可变的（存入的值的内容和长度都是可改变的，且引用地址不变），它不是线程安全的。
- * - StringBuffer**：StringBuffer也是可变的，它是线程安全的，所以它的开销比StringBuilder大
+ * - String：String对象是不可变的。对String对象的任何改变都不影响到原对象的内容，
+ *          相关的任何change操作都会导致该字符串变量指向新的对象的地址。
+ * - StringBuilder：StringBuilder是可变的（存入的值的内容和长度都是可改变的，且引用地址不变），它不是线程安全的。
+ * - StringBuffer：StringBuffer也是可变的，它是线程安全的，所以它的开销比StringBuilder大
  */
 public final class StringContrast {
 
@@ -32,7 +30,8 @@ public final class StringContrast {
         }
         str = null;
         System.gc();//进行一次垃圾回收，避免缓存对后续测试的影响。
-        System.out.println("str          累加" + frequency + "个长度为1的字符串,平均耗时为：" + totalTime / cycle);
+        System.out.println("str          累加" + frequency + "个长度为1的字符串,平均耗时为："
+                + totalTime / cycle);
 
         totalTime = i = 0;
         for (int j = 0; j < cycle; j++) {
@@ -45,7 +44,8 @@ public final class StringContrast {
         }
         stringBuffer = null;
         System.gc();
-        System.out.println("stringBuffer 累加" + frequency + "个长度为1的字符串,平均耗时为：" + totalTime / cycle);
+        System.out.println("stringBuffer 累加" + frequency + "个长度为1的字符串,平均耗时为："
+                + totalTime / cycle);
 
         totalTime = i = 0;
         for (int j = 0; j < cycle; j++) {
@@ -58,7 +58,8 @@ public final class StringContrast {
         }
         stringBuilder = null;
         System.gc();
-        System.out.println("stringBuilder累加" + frequency + "个长度为1的字符串,平均耗时为：" + totalTime / cycle);
+        System.out.println("stringBuilder累加" + frequency + "个长度为1的字符串,平均耗时为："
+                + totalTime / cycle);
 
         System.out.println();
     }
