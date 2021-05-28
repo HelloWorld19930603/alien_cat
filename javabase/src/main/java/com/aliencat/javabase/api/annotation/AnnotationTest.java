4ba048b81aa2a09c7452cc04fdafc77d96c26712
HelloWorld19930603 <784264581@qq.com>
2021-05-28 14:55:07 +0800

    注解Annotation

---
+++ javabase/src/main/java/com/aliencat/javabase/api/annotation/AnnotationTest.java
@@ -0,0 +1,32 @@
+package com.aliencat.javabase.api.annotation;
+
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class AnnotationTest {
+
+    @Test
+    public void test1() throws NoSuchMethodException {
+        Class cs = Info.class;
+        //获取run方法对象
+        Method method = cs.getMethod("run");
+        //获取run方法上注解，注意只能得到@Retention值为RetentionPolicy.RUNTIME的注解
+        Annotation[] annotations = method.getAnnotations();
+        for (Annotation an : annotations) {
+            System.out.println(an.toString());
+            //判断是否是指定Annotation
+        }
+
+        //判断是否包含指定Annotation
+        if (method.isAnnotationPresent(MyAnnotation.class)) {
+            //获取指定Annotation
+            MyAnnotation myAnnotation = method.getAnnotation(MyAnnotation.class);
+            //获取Annotation的属性
+            String key = myAnnotation.key();
+            String value = myAnnotation.value();
+            System.out.println(key + " --> " + value);
+        }
+    }
+}
---
+++ javabase/src/main/java/com/aliencat/javabase/api/annotation/MyAnnotation.java
@@ -0,0 +1,49 @@
+package com.aliencat.javabase.api.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface MyAnnotation {
+    public String key() default "default_key";
+    public String value() default "default_value";
+}
+
+@MyAnnotation
+interface inter{
+    public void run();
+}
+
+@MyAnnotation
+@interface MyAnnotationA{
+
+}
+
+//定义类上
+@MyAnnotation
+class Info implements inter{
+    //定义在属性上
+    @MyAnnotation
+    public static String desc = "info";
+
+    //定义在构造器上
+    @MyAnnotation
+    Info(){
+        System.out.println("InheritableFather:"+Info.class.isAnnotationPresent(Inherited.class));
+    }
+
+    //定义在方法和参数上
+    @MyAnnotation
+    public static void main(@MyAnnotation String[] args) {
+        System.out.println(desc);
+    }
+
+    @Override
+    @Deprecated
+    @SuppressWarnings({"deprecated","unused"})
+    @MyAnnotation
+    public void run() {
+
+    }
+}
