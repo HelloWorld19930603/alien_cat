ba7c7f2c549ca7ab2fb44b84e9239cd613b9c15c
HelloWorld19930603 <784264581@qq.com>
2021-06-14 16:19:06 +0800

    自定义starter的demo

---
+++ springbootdemo/src/main/java/com/aliencat/springboot/demo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.aliencat.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
---
+++ springbootdemo/src/test/java/com/aliencat/springboot/demo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliencat.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
