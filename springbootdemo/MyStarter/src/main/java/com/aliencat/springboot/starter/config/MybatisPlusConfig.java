d989b03e4d35ffbd28b27e746fe243f90a25950f
HelloWorld19930603 <784264581@qq.com>
2021-06-14 16:55:08 +0800

    自定义starter的demo

---
+++ springbootdemo/MyStarter/src/main/java/com/aliencat/springboot/starter/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.aliencat.springboot.starter.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * mybatis plus分页插件配置
+ * @author wj
+ */
+@ConditionalOnClass(value = {PaginationInterceptor.class})
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}
