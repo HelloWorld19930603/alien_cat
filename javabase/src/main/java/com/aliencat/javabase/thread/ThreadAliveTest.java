cc1535249d6a75e5729f84fd2106834043d07ed1
HelloWorld19930603 <784264581@qq.com>
2021-06-25 16:05:37 +0800

    功能测试

--- javabase/src/main/java/com/aliencat/javabase/thread/ThreadAliveTest.java
+++ javabase/src/main/java/com/aliencat/javabase/thread/ThreadAliveTest.java
@@ -11,7 +11,7 @@ public class ThreadAliveTest {
      * 而当我们线程执行完毕后将会调用exit方法，该方法将会调用ensure_join方法，
      * 在该方法中将eetop甚至为null，但是由于赋值前需要获取到Java线程的对象锁，
      * 而该对象的对象锁已经由线程T2持有，这时当前线程将会阻塞，从而造成eetop变量没有被清除，
-     * 从而导致isAlive方法在T1线程执行完毕后仍然返回true。读者也可以看看java Thread的源码，
+     * 从而导致isAlive方法在T1线程执行完毕后仍然返回true。
      * join函数也是通过对Thread对象获取锁然后调用isAlive来判定线程是否结束的，
      * 这就意味着如果我们用别的线程持有了Java Thread的对象锁，那么这时调用join方法的线程也是会被阻塞的。
      */
