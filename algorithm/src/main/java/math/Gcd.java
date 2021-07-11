46b33a2cdbe1dd5bc391e177690f22867a2f64ec
HelloWorld19930603 <784264581@qq.com>
2021-07-11 16:16:54 +0800

    求最大公约数

---
+++ algorithm/src/main/java/math/Gcd.java
@@ -0,0 +1,26 @@
+package math;
+
+import java.util.Scanner;
+
+public class Gcd {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);//以空格作为分隔符
+        System.out.println("请输入a:");
+        int a = scan.nextInt();
+        System.out.println("请输入b:");
+        int b = scan.nextInt();
+        int m;
+        for (; ; ) {
+            m = a % b;
+            if (m == 0)
+                break;
+            else {
+                a = b;
+                b = m;
+            }
+        }
+        System.out.println("最大公约数为：" + b);
+    }
+}
