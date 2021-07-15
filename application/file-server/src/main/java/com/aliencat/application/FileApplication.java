cd5fb0516901a8209f0788220ccc07ee83559109
HelloWorld19930603 <784264581@qq.com>
2021-07-15 22:44:39 +0800

    文件上传

---
+++ application/file-server/src/main/java/com/aliencat/application/FileApplication.java
@@ -0,0 +1,12 @@
+package com.aliencat.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
---
+++ application/file-server/src/main/java/com/aliencat/application/controller/DownLoadController.java
@@ -0,0 +1,73 @@
+package com.aliencat.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+
+@Controller
+public class DownLoadController {
+    private final static String utf8 = "utf-8";
+
+    @RequestMapping("/download")
+    public void downLoadFile(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        File file = new File("C:\\Users\\admin\\nginx原理.mp4");
+        response.setCharacterEncoding(utf8);
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            //分片下载   http  Range bytes=100-1000   bytes=100-
+            long fSize = file.length();
+            response.setContentType("application/x-download");
+            String fileName = URLEncoder.encode(file.getName(), utf8);
+            response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.setHeader("Accept-Range", "bytes");
+
+            response.setHeader("fSize", String.valueOf(fSize));
+            response.setHeader("fName", fileName);
+
+            long pos = 0, last = fSize - 1, sum = 0;
+            if (null != request.getHeader("Range")) {
+                response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+
+                String numRange = request.getHeader("Range").replaceAll("bytes=", "");
+                String[] strRange = numRange.split("-");
+                if (strRange.length == 2) {
+                    pos = Long.parseLong(strRange[0].trim());
+                    last = Long.parseLong(strRange[1].trim());
+                    if (last > fSize - 1) {
+                        last = fSize - 1;
+                    }
+                } else {
+                    pos = Long.parseLong(numRange.replaceAll("-", "").trim());
+                }
+            }
+            long rangeLenght = last - pos + 1;
+            String contentRange = new StringBuffer("bytes ").append(pos).append("-").append(last).append("/").append(fSize).toString();
+            response.setHeader("Content-Range", contentRange);
+            response.setHeader("Content-Lenght", String.valueOf(rangeLenght));
+
+            os = new BufferedOutputStream(response.getOutputStream());
+            is = new BufferedInputStream(new FileInputStream(file));
+            is.skip(pos);
+            byte[] buffer = new byte[1024];
+            int lenght = 0;
+            while (sum < rangeLenght) {
+                lenght = is.read(buffer, 0, ((rangeLenght - sum) <= buffer.length ? ((int) (rangeLenght - sum)) : buffer.length));
+                sum = sum + lenght;
+                os.write(buffer, 0, lenght);
+            }
+            System.out.println("下载完成");
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (os != null) {
+                os.close();
+            }
+        }
+    }
+}
