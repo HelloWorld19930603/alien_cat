b5c20cac591b027d7f7aca9f6d2ec7aa04573dff
HelloWorld19930603 <784264581@qq.com>
2021-07-18 21:28:25 +0800

    springboot demo

--- springboot-demo/springboot-redis/src/main/java/com/aliencat/springboot/redis/SpringbootRedisApplication.java
+++ springboot-demo/springboot-redis/src/main/java/com/aliencat/springboot/redis/SpringbootRedisApplication.java
@@ -1,13 +1,12 @@
-package com.aliencat.springboot.demo;
+package com.aliencat.springboot.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class SpringbootRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootdemoApplication.class, args);
+        SpringApplication.run(SpringbootRedisApplication.class, args);
     }
-
 }
---
+++ springboot-demo/springboot-redis/src/main/java/com/aliencat/springboot/redis/controller/Controller.java
@@ -0,0 +1,55 @@
+package com.aliencat.springboot.redis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    /**
+     * 获取缓存
+     */
+    //测试：http://localhost:8080/redis/get/demo
+    @RequestMapping("/redis/get/{key}")
+    public String get(@PathVariable("key") String key) {
+        return template.opsForValue().get(key);
+    }
+
+    /**
+     * 设置缓存
+     */
+    //测试：http://localhost:8080/redis/set/demo/val
+    @RequestMapping("/redis/set/{key}/{value}")
+    public Boolean set(@PathVariable("key") String key, @PathVariable("value") String value) {
+        boolean flag = true;
+        try {
+            template.opsForValue().set(key, value);
+            //有效时长（秒）
+            template.expire(key, 10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            flag = false;
+        }
+
+        return flag;
+    }
+
+    /**
+     * 发布消息
+     */
+    //测试：http://localhost:8080/redis/eventPush
+    @RequestMapping("/redis/eventPush")
+    public Boolean eventPush() {
+        template.convertAndSend("topic1", "topic1-第一种事件消息");
+        template.convertAndSend("topic2", "topic2-第二种事件消息");
+        return true;
+    }
+}
