96d1d5961fc73f167fbffc3ab73a8fbc8c1142e3
HelloWorld19930603 <784264581@qq.com>
2021-07-19 22:44:42 +0800

    lambda使用示例

--- javabase/src/main/java/com/aliencat/javabase/java8/lamda/LambdaDemo.java
+++ javabase/src/main/java/com/aliencat/javabase/java8/lamda/LambdaDemo.java
@@ -2,6 +2,8 @@ package com.aliencat.javabase.java8.lamda;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -59,6 +61,41 @@ public class LambdaDemo {
 
         //使用lambda表达式写法
         new Thread(() -> System.out.println("新线程任务执行！")).start(); // 启动线程
+    }
+
+
+    //遍历map
+    @Test
+    public void testForEach() {
+        Map<String, Integer> items = new HashMap<>(8);
+        items.put("A", 1);
+        items.put("B", 2);
+        items.put("C", 3);
+        items.put("D", 4);
+        items.put("E", 5);
+        items.put("F", 6);
+        items.forEach((k, v) -> {
+            System.out.println(String.format("key : %s , value : %s", k, v));
+        });
+    }
+
+    /**
+     * * 示例：
+     * * 1. 不需要参数,返回值为 6
+     * * () -> 6
+     * * 2. 接收一个参数(数字类型),返回其2倍的值
+     * * x -> 2 * x
+     * * 3. 接受2个参数(数字),并返回他们的差值
+     * * (x, y) -> x – y
+     * * 4. 接收2个int型整数,返回他们的和
+     * * (int x, int y) -> x + y
+     * * 5. 接受一个 string 对象,并在控制台打印,不返回任何值(看起来像是返回void)
+     * * (String s) -> System.out.print(s)
+     */
+    @Test
+    public void testArgs() {
+        Function<Integer, Integer> function = (a) -> a + 2;
+        System.out.println(function.apply(1));
     }
 }
 
