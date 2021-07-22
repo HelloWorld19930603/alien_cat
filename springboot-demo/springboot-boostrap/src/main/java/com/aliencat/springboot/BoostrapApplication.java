cb8466e9041c40cc3f8aa6036cd506606a6c5c87
HelloWorld19930603 <784264581@qq.com>
2021-07-22 14:53:46 +0800

    springboot整合boostrap使用示例

---
+++ springboot-demo/springboot-boostrap/src/main/java/com/aliencat/springboot/BoostrapApplication.java
@@ -0,0 +1,16 @@
+package com.aliencat.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class BoostrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostrapApplication.class, args);
+    }
+
+}
---
+++ springboot-demo/springboot-boostrap/src/main/java/com/aliencat/springboot/controller/QatestController.java
@@ -0,0 +1,31 @@
+package com.aliencat.springboot.controller;
+
+import com.aliencat.springboot.response.JSONResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class QatestController {
+
+    @RequestMapping("/qatest")
+    @ResponseBody
+    public JSONResult qatest(String data) {
+        log.info("data : " + data);
+        try {
+            return new JSONResult(200, "请求成功", data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new JSONResult(500, "数据异常", e.toString());
+        }
+    }
+
+    @GetMapping("/index")
+    public String index() {
+
+        return "index";
+    }
+}
