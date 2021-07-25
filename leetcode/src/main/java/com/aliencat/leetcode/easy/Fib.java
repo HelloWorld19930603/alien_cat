392078d8fd2fa3f96285c871037a7f3d2f5bb5e0
HelloWorld19930603 <784264581@qq.com>
2021-07-25 21:58:08 +0800

    力扣刷题之斐波那契数

---
+++ leetcode/src/main/java/com/aliencat/leetcode/easy/Fib.java
@@ -0,0 +1,40 @@
+package com.aliencat.leetcode.easy;
+
+/**
+ * 509. 斐波那契数
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ * <p>
+ * 示例 1：
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 示例 2：
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 示例 3：
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ */
+public class Fib {
+
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int first = 0;
+        int second = 1;
+        int sum;
+        for (int i = 0; i < n - 1; i++) {
+            sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
+    }
+
+}
