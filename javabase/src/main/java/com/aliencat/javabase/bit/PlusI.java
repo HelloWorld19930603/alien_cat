0c56e6dbb1645f0d55f50549f20b6b4eb19169bf
HelloWorld19930603 <784264581@qq.com>
2021-08-01 21:27:16 +0800

    关于i++的一个小测试

--- javabase/src/main/java/com/aliencat/javabase/bit/PlusI.java
+++ javabase/src/main/java/com/aliencat/javabase/bit/PlusI.java
@@ -3,33 +3,44 @@ package com.aliencat.javabase.bit;
 public class PlusI {
 
 
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i++); //输出0
 
-    public void iPlusPlus(){
+        i = 0;
+        System.out.println(i++ + i++);//输出 1
+
+        i = 0;
+        System.out.println(i++ + ++i);  //输出2
+
+        i = 0;
+        System.out.println(i++ + i++ + i++); //输出3
+
+        i = 0;
+        System.out.println(i++ + i++ + i++ + i++); //输出6
+    }
+
+    public void iPlusPlus() {
         int i = 0;
         int j = i++;
     }
 
-    public int iPlusPlus2(){
+    public int iPlusPlus2() {
         int i = 0;
         return i++;
     }
 
-    public void iPlusPlus3(){
+    public void iPlusPlus3() {
         int i = 6;
         int a = 3;
         a = i;
         int b = 4;
         b = i;
-        i = i+2;
+        i = i + 2;
     }
 
-    public void plusPlusI(){
+    public void plusPlusI() {
         int i = 0;
         int j = ++i;
-    }
-
-    public static void main(String[] args) {
-        int i = 0;
-        System.out.println(i++ + ++i);
     }
 }
