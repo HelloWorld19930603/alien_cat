fc67d5918f452698cbabe3124954c34bccbfc75a
HelloWorld19930603 <784264581@qq.com>
2021-08-12 22:00:38 +0800

    springmvc整合过滤器测试

---
+++ springboot-demo/springboot-ssm/src/main/java/com/aliencat/springboot/ssm/filter/TestFilter.java
@@ -0,0 +1,32 @@
+package com.aliencat.springboot.ssm.filter;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//配置拦截路径
+@WebFilter(filterName = "testFilter", urlPatterns = {"/test"})
+@ServletComponentScan
+@Component
+public class TestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        System.out.println("RequestURI：" + request.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
